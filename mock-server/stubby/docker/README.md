8722565a1f30bf7a8174e8746cf13e75b78ec9c9
Patrick <patrickwuke@163.com>
2022-07-18 21:35:25 +0800

    add qbox server

--- mock-server/stubby/docker/README.md
+++ mock-server/stubby/docker/README.md
@@ -1,6 +1,13 @@
 # README
 
-- 
-- admin page: 
+
+# API Stubby
+
+Use Stubby4j to mock http2 server
+- admin page:
   - http://0.0.0.0:8891/status
--  
+
+
+## Resources
+
+- [stubby4j](https://github.com/azagniotov/stubby4j.git)
---
+++ qboxserver/src/main/java/io/fluent/qboxserver/QBoxApp.java
@@ -0,0 +1,16 @@
+package io.fluent.qboxserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import xyz.erupt.core.annotation.EruptScan;
+
+@SpringBootApplication
+@EntityScan
+@EruptScan
+public class QBoxApp {
+    public static void main(String[] args) {
+        SpringApplication.run(QBoxApp.class);
+    }
+}
